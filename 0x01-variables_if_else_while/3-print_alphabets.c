b96025d466141e4a2a6e989cca947fdae0e142c8
Goodness-theodore <goodness.theodore@gmail.com>
2023-08-11 16:45:17 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,10 +12,13 @@
 int main(void)
 
 {
-        char low;
-        for (low = 'a'; low <= 'z'; low++)
+
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
         putchar(low);
-        for (low = 'A'; low <= 'Z'; low++)
+
+	for (low = 'A'; low <= 'Z'; low++)
         putchar(low);
         putchar('\n');
 

0cce75217ad1c7b9607599fe39360c902cb63a2a
Goodness-theodore <goodness.theodore@gmail.com>
2023-08-15 18:51:50 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main: 'A program that prints _putchar'
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}

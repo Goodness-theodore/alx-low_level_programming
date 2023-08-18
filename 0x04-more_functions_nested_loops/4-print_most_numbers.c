b255b581dcf288ad1b4644bbd81ba2963cf1c2ca
Goodness-theodore <goodness.theodore@gmail.com>
2023-08-18 14:03:40 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9
+ *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		if (x != 50)
+		{
+			if (x != 52)
+			{
+				_putchar(x);
+			}
+		}
+	}
+	_putchar('\n');
+}

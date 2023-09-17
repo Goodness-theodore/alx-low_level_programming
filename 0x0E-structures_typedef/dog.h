59265130b210cd9872eb151815c8b1d58bdbc7b1
Goodness-theodore <goodness.theodore@gmail.com>
2023-09-17 06:29:11 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,12 +14,15 @@ struct dog
 	char *owner;
 
 };
+
+/**
+ * dog_t - New name for the type
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
 #endif
